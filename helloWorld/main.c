d9fcb02b428ca47c184026b7d896f6cdfe28c332
William Thomas Perkins <williamthomasperkins@gmail.com>
2016-02-17 10:27:58 +0000

    Added new printf statemetns

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -2,7 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello world");
+	printf("Hello world")
+	
+	
+	printf("Hello world 2");
+	printf("Helloworld 3");
+	printf("Hello world 4");
+	printf("Hello world 5");
+	
 	
 	printf("Goodbye World");
 	

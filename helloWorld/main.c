734d79282df2d316bd49cb72591790ae648101a9
Josh <joshua.john1337@gmail.com>
2016-02-17 10:50:23 +0000

    Update main.c
    very important, much needed notes added

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -2,19 +2,26 @@
 
 int main(int argc, char *argv[])
 {
+	//Prints "Hello world"
 	printf("Hello world\n");
 	
-	
+	//Prints "Hello world 2"
 	printf("Hello world 2\n");
+	//Prints "Hello world 3"
 	printf("Hello world 3\n");
+	//Prints "Hello world 4"
 	printf("Hello world 4\n");
+	//Prints "Hello world 5"
 	printf("Hello world 5\n");
 	
 	
+	//Prints "Goodbye World"
 	printf("Goodbye World\n");
 	
+	//Prints "Test"
 	printf("Test\n");
 	
+	//Prints "World Domination Achieved!"
 	printf("World Domination Achieved!")
 	return 0;
 }

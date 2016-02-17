44892f964458f3fb37b304019f5b39e5b7ed64cb
William Thomas Perkins <williamthomasperkins@gmail.com>
2016-02-17 10:29:21 +0000

    Made some hotfix changes

--- helloWorld/main.c
+++ helloWorld/main.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello world")
+	printf("Hello world");
 	
 	
 	printf("Hello world 2");
-	printf("Helloworld 3");
+	printf("Hello world 3");
 	printf("Hello world 4");
 	printf("Hello world 5");
 	
